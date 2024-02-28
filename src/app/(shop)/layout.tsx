5206585d5415b19e4655eb52b1f2f21eff6ef3f6
Juan <gurquiza8@gmail.com>
2024-02-27 19:16:50 -0600

    Title component

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -10,7 +10,10 @@ export default function ShopLayout({
 
             <TopMenu />
 
-            { children }
+            <div className="px-0 sm:px-10">
+                { children }
+            </div>
+
         </main>
     )
 }
