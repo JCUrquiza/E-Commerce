8936e8d7f50e44694823e19cf8baac85e2d3236b
Juan <gurquiza8@gmail.com>
2024-03-02 14:10:31 -0600

    Desing sidemenu component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@
 export * from './ui/not-found/PageNotFound';
 export * from './ui/top-menu/TopMenu';
 export * from './ui/title/Title';
+export * from './ui/sidebar/Sidebar';
 
 export { ProductGrid } from './products/product-grid/ProductGrid';
 export { ProductGridItem } from './products/product-grid/ProductGridItem';
