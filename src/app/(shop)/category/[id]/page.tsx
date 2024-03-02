01ae82e6246e2b4a91977f06e4bdfba33a062121
Juan <gurquiza8@gmail.com>
2024-03-02 15:51:34 -0600

    Show products by categories

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,23 +1,42 @@
-import { notFound } from 'next/navigation';
 import React from 'react';
+import { notFound } from 'next/navigation';
+import { initialData } from '@/seed/seed';
+import { ProductGrid, Title } from '@/components';
+import { Category } from '@/interfaces';
+
+const seedProducts = initialData.products;
 
 interface Props {
     params: {
-        id: string;
+        id: Category;
     }
 }
 
 export default function ({ params }: Props) {
 
     const { id } = params;
+    const products = seedProducts.filter( product => product.gender === id );
 
-    if ( id === 'kids' ) {
-        notFound();
-    }    
+    const labels: Record<Category, string> = {
+        'men': 'para hombres',
+        'women': 'para mujeres',
+        'kid': 'para niños',
+        'unisex': 'para Todos'
+    }
+
+    // if ( id === 'kids' ) {
+    //     notFound();
+    // }    
 
     return (
-        <div>
-            <h1>Category Page { id }</h1>
-        </div>
+        <>
+            <Title
+                title={`Artículos de ${ labels[id] }`}
+                subtitle={`Productos de ${ id }`}
+                className='mb-2'
+            />
+
+            <ProductGrid products={ products } />
+        </>
     )
 }
