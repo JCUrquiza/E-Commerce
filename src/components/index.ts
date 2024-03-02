dcc3a76cb65ddda92e590ba85b1bdbc9ef6155d8
Juan <gurquiza8@gmail.com>
2024-03-02 13:34:41 -0600

    Building grid for products and charge the image into card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,6 @@ export * from './ui/not-found/PageNotFound';
 export * from './ui/top-menu/TopMenu';
 export * from './ui/title/Title';
 
+export { ProductGrid } from './products/product-grid/ProductGrid';
+export { ProductGridItem } from './products/product-grid/ProductGridItem';
+
