c1588c53e302440b153fd6050b1faf01263423c6
Juan <gurquiza8@gmail.com>
2024-03-03 15:54:34 -0600

    Finish seed for products, productsImage and Categories

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -12,18 +12,48 @@ async function main() {
 
     const { categories, products } = initialData;
 
-    // Categorias
+    // 2. Insertar categorias
     const  categoriesData = categories.map( category => ({
         name: category
     }));
     await prisma.category.createMany({ data: categoriesData });
 
+    // Crear objeto de categorias: { 'nombre': 'id' }
+    const categoriesDB = await prisma.category.findMany();
+    const categoriesMap = categoriesDB.reduce( (map, category) => {
+        map[ category.name.toLowerCase() ] = category.id
+        return map;
+    }, {} as Record<string, string>);   // <string=shirt, string=categoryDB>
+
+    // 3. Insertar Product e ProductImage
+    products.forEach( async(product) => {
+
+        // Product
+        const { type, images, ...rest } = product;
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        });
+
+        // ProductImage
+        const imagesData = images.map( image => ({
+            url: image,
+            productId: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        });
+
+    });
+
+    
 
 
 
 
-
-    console.log(initialData);
     console.log('Seed ejecutado correctamente');
 }
 
