c92836f89d5d2184924949cc3b2c33bb780b7036
Juan <gurquiza8@gmail.com>
2024-03-03 12:47:23 -0600

    Building login and register page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -62,7 +62,7 @@ export default function () {
                     </div>
 
                     {/* Checkout - Resumen de órden */}
-                    <div className='bg-white rounded-xl shadow-xl p-7'>
+                    <div className='bg-white rounded-xl shadow-xl p-7 h-[300px]'>
                         <h2 className='text-2xl mb-2'>Resumen de órden</h2>
 
                         <div className='grid grid-cols-2'>
