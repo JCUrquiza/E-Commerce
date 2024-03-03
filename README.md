f0956ee0ad4375d92fa1961083ec54d13148be86
Juan <gurquiza8@gmail.com>
2024-03-03 14:55:45 -0600

    Creating model to category product with prisma

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 2. Crear una copia del ```.env.template``` y renombrarlo a ```.env``` y cambiar las variables de entorno
 3. Instalas dependencias ```npm install```
 4. Levantar la base de datos ```docker compose up -d```
-5. Correr el proyecto ```npm run dev```
+5. Correr las migraciones de Prisma ```npx prisma migrate dev```
+6. Ejecutar el seed ```npm run seed```
+6. Correr el proyecto ```npm run dev```
 
 
 ## Correr en prod
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,34 @@
+import prisma from '../lib/prisma';
+import { initialData } from './seed';
+
+async function main() {
+
+    // 1. Borrar registros previos
+    await Promise.all([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
+    ]);
+
+    const { categories, products } = initialData;
+
+    // Categorias
+    const  categoriesData = categories.map( category => ({
+        name: category
+    }));
+    await prisma.category.createMany({ data: categoriesData });
+
+
+
+
+
+
+    console.log(initialData);
+    console.log('Seed ejecutado correctamente');
+}
+
+
+(() => {
+    if (process.env.NODE_ENV === 'production') return;
+    main();
+})();
