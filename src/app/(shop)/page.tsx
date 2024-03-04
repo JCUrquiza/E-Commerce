8c7467bd9528ae21a0fc04ff83f893db0b34c635
Juan <gurquiza8@gmail.com>
2024-03-03 19:18:49 -0600

    Products page with pagination using search params

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,15 +1,25 @@
+import { redirect } from 'next/navigation';
 import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid, Title } from '@/components';
-import { initialData } from '@/seed/seed';
 
-const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string;
+  }
+}
 
-export default async function Home() {
+export default async function Home({ searchParams }: Props) {
 
-  const productsTemp = await getPaginatedProductsWithImages();
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({ page });
+
+  if ( products.length === 0 ) {
+    redirect('/');
+  }
 
   return (
     <>
+
       <Title title='Tienda' subtitle='Todos los productos' className='mb-2' />
 
       <ProductGrid products={products} />
