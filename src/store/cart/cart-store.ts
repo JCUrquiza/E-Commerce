ef52319c2697419aa6e17376270b6ff1e5853757
Juan <gurquiza8@gmail.com>
2024-03-08 14:18:23 -0600

    solving hydratation problem in render quantity product in topmenu

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,44 +1,62 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import type { CartProduct } from '@/interfaces';
 
 interface State {
     cart: CartProduct[];
 
     // Methods
+    getTotalItems: () => number;
     addProductToCart: (product: CartProduct) => void;
     // updateProductQuantity
     // removeProduct
 }
 
 export const useCartStore = create<State>()(
-    (set, get) => ({
-        cart: [],
-        addProductToCart: (product: CartProduct) => {
-            // Desestructuración
-            const { cart } = get();
-            console.log(cart);
-            
-            // 1.- Revisar si el producto existe en el carrito con la talla seleccionada. (some regresa true o false y ya no continua):
-            const productInCart = cart.some(
-                (item) => (item.id === product.id && item.size === product.size)
-            );
-            if (!productInCart) {
-                set({
-                    cart: [...cart, product]
-                });
-                return;
-            }
 
-            // 2.- Se que el producto existe por talla... tengo que incrementar
-            const updatedCartProducts = cart.map( item => {
-                if ( item.id === product.id && item.size === product.size ) {
-                    return {...item, quantity: item.quantity +  product.quantity };
+    persist(
+
+        (set, get) => ({
+
+            cart: [],
+
+            getTotalItems: () => {
+                const { cart } = get();
+                return cart.reduce((total, item) => total + item.quantity, 0);
+            },
+
+            addProductToCart: (product: CartProduct) => {
+                // Desestructuración
+                const { cart } = get();
+                console.log(cart);
+                
+                // 1.- Revisar si el producto existe en el carrito con la talla seleccionada. (some regresa true o false y ya no continua):
+                const productInCart = cart.some(
+                    (item) => (item.id === product.id && item.size === product.size)
+                );
+                if (!productInCart) {
+                    set({
+                        cart: [...cart, product]
+                    });
+                    return;
                 }
-                return item;
-            });
+    
+                // 2.- Se que el producto existe por talla... tengo que incrementar
+                const updatedCartProducts = cart.map( item => {
+                    if ( item.id === product.id && item.size === product.size ) {
+                        return {...item, quantity: item.quantity +  product.quantity };
+                    }
+                    return item;
+                });
+    
+                set({ cart: updatedCartProducts });
+    
+            }
+        })
 
-            set({ cart: updatedCartProducts });
-
+        ,{
+            name: 'shopping-cart',
         }
-    })
+    )
+
 )
