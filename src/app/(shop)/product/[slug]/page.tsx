4396688fdf3426c833761164d87d10fa069541c7
Juan <gurquiza8@gmail.com>
2024-03-08 12:50:40 -0600

    Creating leaf for size of product details

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { titleFont } from '@/config/fonts';
 import { ProductMovileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from '@/components';
 import { getProductBySlug } from '@/actions';
 import { Metadata, ResolvingMetadata } from 'next';
+import { AddToCart } from './ui/AddToCart';
 // import { getProductBySlug } from '@/actions/product/get-product-by-slug';
 
 interface Props {
@@ -77,16 +78,7 @@ export default async function ProductsBySlugPage({ params }: Props) {
 
                 <p className='text-lg mb-5'>${ product.price.toFixed(2) }</p>
 
-                {/* Selector de Tallas */}
-                <SizeSelector selectedSize={ product.sizes[0] } availableSizes={ product.sizes } />
-
-                {/* Selector de Cantidad */}
-                <QuantitySelector quantity={2} />
-
-                {/* Botón */}
-                <button className='btn-primary my-5'>
-                    Agregar al carrito
-                </button>
+                <AddToCart product={product} />
 
                 {/* Descripción */}
                 <h3 className='font-bold text-sm'>Descripción</h3>
