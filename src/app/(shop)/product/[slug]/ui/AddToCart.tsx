5d2cffbcdb6251305b6bf1f36af583336e339f6f
Juan <gurquiza8@gmail.com>
2024-03-08 13:06:32 -0600

    Creating leaf for quantity of product details

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -11,17 +11,30 @@ interface Props {
 export const AddToCart = ({ product }: Props) => {
 
     const [size, setSize] = useState<Size | undefined>();
+    const [quantity, setQuantity] = useState<number>(1);
+    const [posted, setPosted] = useState(false);
+
+    const addToCart = () => {
+        setPosted(true);
+        if (!size) return;
+        console.log({ size, quantity });
+    }
 
     return (
         <>
+
+            {
+                posted && !size && <span className='mt-2 text-red-500 fade-in'>Debe de seleccionar una talla*</span>
+            }
+            
             {/* Selector de Tallas */}
             <SizeSelector selectedSize={ size } availableSizes={ product.sizes } onSizeChanged={ size => setSize(size)} />
 
             {/* Selector de Cantidad */}
-            <QuantitySelector quantity={2} />
+            <QuantitySelector quantity={quantity} onQuantityChanged={ quantity => setQuantity(quantity) } />
 
             {/* Botón */}
-            <button className='btn-primary my-5'>
+            <button className='btn-primary my-5' onClick={ addToCart }>
                 Agregar al carrito
             </button>
         </>
