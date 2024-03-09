16104b574dcbc8074a0cbd87a8720e157cdff17f
Juan <gurquiza8@gmail.com>
2024-03-09 15:12:11 -0600

    Adding next-auth library and auth-configuration file

---
+++ src/auth.config.ts
@@ -0,0 +1,10 @@
+import { NextAuthConfig } from 'next-auth';
+
+export const authConfig: NextAuthConfig = {
+    pages: {
+        signIn: '/auth/login',
+        newUser: '/auth/new-account'
+    },
+    providers: []
+}
+
