a161eea2a17793998e2ad04066eed483426df666
Juan <gurquiza8@gmail.com>
2024-03-11 19:03:14 -0600

    Implementing form validations with react hook form

---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import clsx from 'clsx';
+
+type FormInputs = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const RegisterForm = () => {
+
+    const { register, handleSubmit, formState: {errors} } = useForm<FormInputs>();
+
+    const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+
+        const { name, email, password } = data;
+        console.log({name, email, password});
+        // Server action
+    }
+
+    return (
+        <form onSubmit={ handleSubmit(onSubmit) } className="flex flex-col">
+
+            {/* {
+                errors.name?.type === 'required' && (
+                    <span className='text-red-500'>El nombre es obligatorio</span>
+                )
+            } */}
+
+            <label htmlFor="email">Nombre completo</label>
+            <input 
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.name
+                        }
+                    )
+                }
+                autoFocus
+                type="text" 
+                {...register('name', { required: true }) }
+            />
+
+            <label htmlFor="email">Correo electrónico</label>
+            <input 
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.email
+                        }
+                    )
+                }
+                type="email" 
+                {...register('email', { required: true, pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ }) }
+            />
+
+            <label htmlFor="email">Contraseña</label>
+            <input
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.password
+                        }
+                    )
+                }
+                type="password" 
+                {...register('password', { required: true, minLength: 6 })}
+            />
+
+            <button className="btn-primary">
+                Crear cuenta
+            </button>
+
+            {/* divisor l ine */ }
+            <div className="flex items-center my-5">
+                <div className="flex-1 border-t border-gray-500"></div>
+                <div className="px-2 text-gray-800">O</div>
+                <div className="flex-1 border-t border-gray-500"></div>
+            </div>
+
+            <Link
+                href="/auth/login" 
+                className="btn-secondary text-center">
+            Ingresar
+            </Link>
+
+        </form>
+    )
+
+}
