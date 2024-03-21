879e4bb67eab86a462e361b2f5461b0c48b960b8
Juan <gurquiza8@gmail.com>
2024-03-20 19:30:44 -0600

    Obtein quantities from products, preparing to save in bbdd

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@/auth.config';
 import type { Address, Size } from '@/interfaces';
+import prisma from '@/lib/prisma';
 
 interface ProductToOrder {
     productId: string;
@@ -21,6 +22,38 @@ export const placeOrder = async( productIds: ProductToOrder[], address: Address 
         }
     }
 
-    console.log({ productIds, address, userId });
+    // Obtener la información de los productos
+    // Nota: Se puede llevar +2 productos con el mismo ID
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: productIds.map( p => p.productId)
+            }
+        }
+    });
+
+    // Calcular los montos
+    const itemsInOrder = productIds.reduce( (count, p) => count + p.quantity, 0 );
+
+    // Los totales de tax, subTotal y total
+    const { subTotal, tax, total } = productIds.reduce( (totals, item) => {
+
+        const productQuantity = item.quantity;
+
+        const product = products.find( product => product.id === item.productId );
+        if ( !product ) throw new Error('Este producto no existe');
+
+        const subTotal = product.price * productQuantity;
+
+        totals.subTotal += subTotal;
+        totals.tax += subTotal * 0.15;
+        totals.total += subTotal * 1.15;
+
+        return totals;
+    }, { subTotal: 0, tax: 0, total: 0 })
+
+    // Crear la transacción de bbdd
+
+    console.log({subTotal, tax, total});
 
 }
